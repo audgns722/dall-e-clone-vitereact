ccdc980f0b984e2f53d3bf44f4238fa1c28f5b3f
audgns722 <audgns722@gmail.com>
2024-01-13 18:23:36 +0900

    firts commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# client
+`npm create vite@latest`
+`npm install`
+`npm install -D tailwindcss postcss autoprefixer`
+`npx tailwindcss init -p`
+`npm install file-saver`
+`npm install react-router-dom`
+
+
+# server
+`npm init -y`
+`npm install cloudinary cors dotenv express mongoose mongodb nodemon openai`
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,39 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+router.route("/").get((req, res) => {
+  res.send("hello dalle");
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: "256x256", // 해상도 변경
+      model: "dall-e-2", // 모델 변경
+      response_format: "b64_json",
+    });
+
+    const image = aiResponse.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
+});
+
+export default router;
